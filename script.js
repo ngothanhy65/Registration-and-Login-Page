a97d54e3c9660668d741c4392d740119908796c1
ngothanhy <ngothanhy54.ct.ct@gmail.com>
2024-08-12 20:50:16 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const registerBtn= document.getElementById('register')
+const loginBtn= document.getElementById('login')
+const container= document.getElementById('container')
+registerBtn.addEventListener("click", ()=>{
+    container.classList.add("right-panel-active")
+});
+loginBtn.addEventListener("click", ()=>{
+    container.classList.remove("right-panel-active")
+});
